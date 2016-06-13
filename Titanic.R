d67d85d9a8b2821679b48ea7dd872bcdc5141217
ajamanu <amanu@champequity.com>
2016-06-13 12:32:11 +1000

    Test commit of script

---
+++ Titanic.R
@@ -0,0 +1,15 @@
+# Titanic.R
+# Script for Kaggle, Titanic project
+# Created by Aja Manu 13/06/2016
+
+# Clear working environment
+rm(list = ls())
+
+# Set working directory
+setwd("C:/Users/amanu/Documents/R/Kaggle/Titanic")
+
+# Load data
+titanic_train <- read.csv("train.csv") # training set
+titanic_test <- read.csv("test.csv") # test set
+
+
